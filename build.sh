69fe89d25f122d63e905dbd05acc70b4106fcc0f
Gabriel Valeriano <53843213+Lummiell@users.noreply.github.com>
2022-09-19 12:34:31 -0300

    Melhorar script de build para bash

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-proj_root=$(pwd) 
+set -e
 
+proj_root=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 hw_dir=$proj_root/hello-world
 elec_hw_dir=$hw_dir/electron-hello-world
 tauri_hw_dir=$hw_dir/tauri-hello-world
@@ -16,20 +17,32 @@ tauri_lottie_dir=$lottie_dir/tauri-lottie-demo
 
 echo "Instalando Dependências..."
 
-cd $elec_hw_dir && npm i
-cd $elec_img_dir && npm i
-cd $elec_lottie_dir && npm i 
+cd $elec_hw_dir
+npm i
+cd $elec_img_dir
+npm i
+cd $elec_lottie_dir
+npm i
 
-cd $tauri_hw_dir && npm i
-cd $tauri_img_dir && npm i
-cd $tauri_lottie_dir && npm i   
+cd $tauri_hw_dir
+npm i
+cd $tauri_img_dir
+npm i
+cd $tauri_lottie_dir
+npm i
 
 echo "Construindo pacotes"
 
-cd $elec_hw_dir && npm run make -- --targets=@electron-forge/maker-deb
-cd $elec_img_dir && npm run make -- --targets=@electron-forge/maker-deb
-cd $elec_lottie_dir && npm run make -- --targets=@electron-forge/maker-deb
+cd $elec_hw_dir
+npm run make -- --targets=@electron-forge/maker-deb
+cd $elec_img_dir
+npm run make -- --targets=@electron-forge/maker-deb
+cd $elec_lottie_dir
+npm run make -- --targets=@electron-forge/maker-deb
 
-cd $tauri_hw_dir && npm run tauri build 
-cd $tauri_img_dir && npm run tauri build
-cd $tauri_lottie_dir && npm run tauri build
+cd $tauri_hw_dir
+npm run tauri build
+cd $tauri_img_dir
+npm run tauri build
+cd $tauri_lottie_dir
+npm run tauri build
